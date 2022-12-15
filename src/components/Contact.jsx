7b01f0e759bd652149f8f0246928956d2e73f8d5
acs777jandb <andy@allsummers.net>
2022-12-15 18:02:06 +0000

    change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ function Contact() {
             <h1 class="font-weight-light">Contact</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
+              inasdasdasdasdsadasdsadsaddustry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
